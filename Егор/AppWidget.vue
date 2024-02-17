8627a2e21da15f92a19d506ed295637ab130059f
TheRisingDream <egor9kaza@mail.ru>
2024-02-17 19:30:10 +0300

    finished the corrections

---
+++ Егор/AppWidget.vue
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+<template>
+<div class="card mb-3" style="max-width: 280px; margin: auto;">  
+<h5 class="title card-title text-center">Программирует на</h5>
+  <div class="row g-0">
+    <div class="d-flex justify-content-around">
+      <img src="../assets/js.jpg" class=" image img-fluid rounded-start" alt="...">
+      <div class="card-body">
+        <p class="card-title">JavaScript <br> работатет с <br> файлом index.js</p>
+      </div>   
+    </div>   
+        <div class="d-flex justify-content-around align-items-center">
+        <p class="time card-title" type="time">12:53:50</p>
+        <button type="button" class="width btn btn-primary ">Стоп</button>
+        <button type="button" class="btn btn-primary btn-sm"><img src="../assets/video-pause-button.png" width="25px"></button>
+        </div> 
+    </div>
+</div>
+</template>
+<style scoped>
+.btn {
+    margin: 5px;
+    font-size: 10px;
+}
+.image{
+    margin: 5px;
+    width: 70px;
+    height: 70px;
+}
+.card{
+    background-color: #3b82f6;
+}
+.time{
+  margin: 5px;
+}
+.card-title{
+  font-weight: bold;
+  font-size: 10px;
+}
+.title{
+  margin: 5px;
+}
+.width{
+  width: 80px;
+}
+</style>
